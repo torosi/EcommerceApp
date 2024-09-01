cb357d3462be4c8b308fa624296a453b57a607da
TOROSI <thomassimons98@gmail.com>
2024-09-01 19:50:03 +0100

    working on category pages

--- EcommerceApp.MVC/Controllers/CategoryController.cs
+++ EcommerceApp.MVC/Controllers/CategoryController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using EcommerceApp.Domain.Dtos;
 using EcommerceApp.Domain.Services.Contracts;
+using EcommerceApp.MVC.Helpers;
 using EcommerceApp.MVC.Models.Category;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -17,16 +19,20 @@ namespace EcommerceApp.MVC.Controllers
         private readonly ILogger<CategoryController> _logger;
         private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
+        private readonly ImageHelper _imageHelper;
 
-        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService, IMapper mapper)
+        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService, IMapper mapper, ImageHelper imageHelper)
         {
             _logger = logger;
             _categoryService = categoryService;
             _mapper = mapper;
+            _imageHelper = imageHelper;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
+            // if there are no categories we still want to display the page? so i am still passing in view models even if empty
             var categoryViewModels = new List<CategoryViewModel>();
 
             try 
@@ -46,6 +52,45 @@ namespace EcommerceApp.MVC.Controllers
             return View(categoryViewModels);
         }
 
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            // if no category found, then redirect to index page
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CategoryViewModel categoryViewModel, IFormFile file)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    var imageUrl = await _imageHelper.UploadImageAsync(file, @"images\category");
+
+                    categoryViewModel.ImageUrl = imageUrl;
+                }
+
+                var categoryDto = _mapper.Map<CategoryDto>(categoryViewModel);
+
+                await _categoryService.AddAsync(categoryDto);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return RedirectToAction("Index");
+        }
 
     }
 }
